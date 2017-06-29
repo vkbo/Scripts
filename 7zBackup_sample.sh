7421a38a3f06e5f2737a00f97eb009764ab6c687
Veronica K. B. Olsen <code@vkbo.net>
2017-06-29 15:36:42 +0200

    Added readme and improved a sample script

--- 7zBackup_sample.sh
+++ 7zBackup_sample.sh
@@ -21,3 +21,48 @@ if [ $(date +%u) = 6 ]; then
 else
     ./7zBackup.sh Sync/Primary DIFF
 fi
+
+#
+#  Secondary Sync Backup
+# =======================
+#  Full: Every month
+#  Diff: Every third day
+#
+
+find $BDIR -name "Secondary.diff" -type f -mtime +32 -delete
+if [ $(date +%d) = 1 ]; then
+    ./7zBackup.sh Sync/Secondary FULL
+fi
+if ! (($(date +%d) % 3)); then
+    ./7zBackup.sh Sync/Secondary DIFF
+fi
+
+#
+#  Tertiary Sync Backup
+# ======================
+#  Full: Every month
+#  Diff: Every week
+#
+
+find $BDIR -name "Tertiary.diff" -type f -mtime +32 -delete
+if [ $(date +%d) = 1 ]; then
+    ./7zBackup.sh Sync/Tertiary FULL
+fi
+if [ $(date +%u) = 6 ]; then
+    ./7zBackup.sh Sync/Tertiary DIFF
+fi
+
+#
+#  Sync Versions Backup
+# ======================
+#  Full: Every month
+#  Diff: Every week
+#
+
+find $BDIR -name "Versions.diff" -type f -mtime +32 -delete
+if [ $(date +%d) = 1 ]; then
+    ./7zBackup.sh Versions FULL
+fi
+if [ $(date +%u) = 6 ]; then
+    ./7zBackup.sh Versions DIFF
+fi
