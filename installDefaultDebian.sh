540dca6bb6f7e7e8336c6e3c3316f91cd21c57be
Veronica K. B. Olsen <code@vkbo.net>
2018-01-19 20:15:06 +0100

    Sorted packages alphabetically

--- installDefaultDebian.sh
+++ installDefaultDebian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo ""
-echo " Running default Debian installs ..."
+echo "Running default Debian installs ..."
 echo "****************************************************************************************************"
 echo ""
 
@@ -16,22 +16,29 @@ else
     cd $HOME/Downloads
 
     # Chrome
+    echo ""
     echo "Installing Google Chrome ..."
+    echo "****************************************************************************************************"
     echo ""
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     dpkg -i google-chrome-stable_current_amd64.deb
+    echo ""
     echo "****************************************************************************************************"
 
     # VSCode
+    echo ""
     echo "Installing Visual Studio Code ..."
+    echo "****************************************************************************************************"
     echo ""
     wget https://go.microsoft.com/fwlink/?LinkID=760868 -O code_latest_amd64.deb
     dpkg -i code_latest_amd64.deb
+    echo ""
     echo "****************************************************************************************************"
     echo ""
 
     # Fix Dependencies
     apt -f install
+    echo ""
     echo "****************************************************************************************************"
     echo ""
     
@@ -41,27 +48,71 @@ else
 fi
 
 # Main Packages
+echo ""
 echo "Installing default packages ..."
+echo "****************************************************************************************************"
 echo ""
 apt update
 apt install -y \
-    geany geany-plugins git meld \
-    gnome-shell-extension-dashtodock gnome-shell-extension-pixelsaver \
-    gnome-shell-extension-system-monitor gnome-shell-extension-weather \
-    texlive-full thunderbird thunderbird-l10n-nb-no keepass2 zim terminator \
-    build-essential autotools-dev automake cmake pkg-config gfortran gcc g++ \
-    python3 ipython ipython3 python3-matplotlib python3-numpy python-matplotlib python-numpy python3-gi \
-    libboost-all-dev libopenmpi-dev libssl-dev \
-    typecatcher vlc gimp inkscape calibre \
-    rar unrar p7zip curl apt-transport-https rsync secure-delete \
+    aptitude \
+    apt-transport-https \
+    automake \
+    autotools-dev \
+    build-essential \
+    calibre \
+    cmake \
+    curl \
+    gcc \
+    geany \
+    geany-plugins \
+    gfortran \
+    gimp \
+    git \
+    gnome-shell-extension-dashtodock \
+    gnome-shell-extension-pixelsaver \
+    gnome-shell-extension-system-monitor \
+    gnome-shell-extension-weather \
+    g++ \
+    inkscape \
+    ipython \
+    ipython3 \
+    keepass2 \
+    libboost-all-dev \
+    libopenmpi-dev \
     libreoffice-style-breeze \
-    lm-sensors aptitude system-config-printer
+    libssl-dev \
+    lm-sensors \
+    meld \
+    pkg-config \
+    python3 \
+    python3-gi \
+    python3-matplotlib \
+    python3-numpy \
+    python-matplotlib \
+    python-numpy \
+    p7zip \
+    rar \
+    rsync \
+    secure-delete \
+    smartmontools \
+    system-config-printer \
+    terminator \
+    texlive-full \
+    thunderbird \
+    thunderbird-l10n-nb-no \
+    typecatcher \
+    unrar \
+    vlc \
+    zim
+echo ""
 echo "****************************************************************************************************"
 echo ""
 
 # Syncthing
 if [ ! -f /etc/apt/sources.list.d/syncthing.list ]; then
+    echo ""
     echo "Installing Syncthing ..."
+    echo "****************************************************************************************************"
     echo ""
     curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
     echo "deb https://apt.syncthing.net/ syncthing stable" | tee /etc/apt/sources.list.d/syncthing.list
@@ -76,7 +127,9 @@ fi
 
 # Spotify
 if [ ! -f /etc/apt/sources.list.d/spotify.list ]; then
+    echo ""
     echo "Installing Spotify ..."
+    echo "****************************************************************************************************"
     echo ""
     apt install -y dirmngr
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
