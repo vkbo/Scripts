a7d01a0c7b45e1bb42305cd42be55c96f3368c7b
Veronica K. B. Olsen <code@vkbo.net>
2018-04-09 14:47:13 +0200

    Made binance script run in a loop (timer is first input parameter) and also calculate first derivative (linear regression)

--- binanceRates.py
+++ binanceRates.py
@@ -7,9 +7,12 @@
 #  URL:     https://github.com/vkbo/Scripts
 #
 
-import json, sys
+import json, sys, os
+import numpy        as np
 
 from urllib.request import Request, urlopen
+from time           import time, sleep
+from datetime       import datetime
 
 # Make JSON API Call
 def getJSON(apiCall):
@@ -19,9 +22,10 @@ def getJSON(apiCall):
     urlData = urlopen(urlReq)
     return json.loads(urlData.read().decode())
 
+# Settings
+maxHist   = 24
+
 # Linux Terminal Colours
-# No idea if these work on windows
-# If not, replace them with empty strings
 RED       = "\033[0;31m"
 GREEN     = "\033[0;32m"
 YELLOW    = "\033[0;33m"
@@ -32,41 +36,76 @@ BOLD      = "\033[0;1m"
 UNDERLINE = "\033[0;4m"
 END       = "\033[0;0m"
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     print("Error: No currency pair specified")
     exit(1)
 else:
-    theCoins = sys.argv[1:]
+    theWait  = float(sys.argv[1])
+    theCoins = sys.argv[2:]
 
+theHist = {}
 for theCoin in theCoins:
-    if theCoin == "":
-        continue
-    apiCall = "http://api.binance.com/api/v1/ticker/24hr?symbol=%s" % theCoin
-    apiJSON = getJSON(apiCall)
+    if not theCoin == "":
+        theHist[theCoin] = []
+
+while True:
     
-    theSymbol = apiJSON["symbol"]
-    lastPrice = float(apiJSON["lastPrice"])
-    lowPrice  = float(apiJSON["lowPrice"])
-    highPrice = float(apiJSON["highPrice"])
-    openPrice = float(apiJSON["openPrice"])
-    change24h = 100*(lastPrice-openPrice)/openPrice
+    toPrint  = "Every %.1f seconds:" % theWait
+    toPrint += " "*(65-len(toPrint))
+    toPrint += datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M:%S')+"\n"
+    toPrint += "\n"
     
-    if   theSymbol[-3:] == "BTC":
-        fmtNum = "%10.8f"
-    elif theSymbol[-3:] == "ETH":
-        fmtNum = "%10.7f"
-    elif theSymbol[-3:] == "BNB":
-        fmtNum = "%10.6f"
-    elif theSymbol[-4:] == "USDT":
-        fmtNum = "%10.4f"
-    else:
-        fmtNum = "%10.6f"
+    for theCoin in theCoins:
+        
+        if theCoin == "":
+            continue
+        
+        apiCall = "http://api.binance.com/api/v1/ticker/24hr?symbol=%s" % theCoin
+        apiJSON = getJSON(apiCall)
+        
+        theSymbol = apiJSON["symbol"]
+        lastPrice = float(apiJSON["lastPrice"])
+        lowPrice  = float(apiJSON["lowPrice"])
+        highPrice = float(apiJSON["highPrice"])
+        openPrice = float(apiJSON["openPrice"])
+        change24h = 100*(lastPrice-openPrice)/openPrice
+        
+        theHist[theCoin].append(lastPrice)
+        
+        nHist = len(theHist[theCoin])
+        yFit  = (0,0)
+        if nHist > 1:
+            xData = np.linspace(0,theWait*(nHist-1),nHist)
+            yFit = np.polyfit(xData,theHist[theCoin],1)
+        if nHist > maxHist:
+            theHist[theCoin].pop(0)
+        
+        if   theSymbol[-3:] == "BTC":
+            fmtNum = "%10.8f"
+        elif theSymbol[-3:] == "ETH":
+            fmtNum = "%10.7f"
+        elif theSymbol[-3:] == "BNB":
+            fmtNum = "%10.6f"
+        elif theSymbol[-4:] == "USDT":
+            fmtNum = "%10.4f"
+        else:
+            fmtNum = "%10.6f"
+        
+        toPrint += (BOLD+"%-8s: "+END) % theSymbol
+        toPrint += (CYAN+fmtNum+" "+END) % lastPrice
+        toPrint += (YELLOW+"(L:"+fmtNum+" H:"+fmtNum+" O:"+fmtNum+") "+END) % (lowPrice,highPrice,openPrice)
+        if change24h > 0:
+            toPrint += (GREEN+"%+7.2f%%"+END) % change24h
+        else:
+            toPrint += (RED+"%+7.2f%%"+END) % change24h
+        toPrint += " [%+7.2f %%/h]" % (360000*yFit[0]/lastPrice)
+        
+        toPrint += "\n"
     
-    toPrint  = (BOLD+"%-8s: "+END) % theSymbol
-    toPrint += (CYAN+fmtNum+" "+END) % lastPrice
-    toPrint += (YELLOW+"(L:"+fmtNum+" H:"+fmtNum+" O:"+fmtNum+") "+END) % (lowPrice,highPrice,openPrice)
-    if change24h > 0:
-        toPrint += (GREEN+"%7.2f%%"+END) % change24h
-    else:
-        toPrint += (RED+"%7.2f%%"+END) % change24h
+    toPrint += "\n"
+    toPrint += "Trend calculated over %.2f seconds. " % (theWait*len(theHist[theCoin]))
+    
+    os.system("clear")
     print(toPrint)
+    sleep(theWait)
+        
