d44625401b025f2f978e3fd1ce7f3cda242a3b75
Veronica K. B. Olsen <code@vkbo.net>
2019-01-09 17:48:33 +0100

    Updates to install script

--- installDefaultDebian.sh
+++ installDefaultDebian.sh
@@ -54,6 +54,7 @@ echo "**************************************************************************
 echo ""
 apt update
 apt install -y \
+    acpi \
     aptitude \
     apt-transport-https \
     automake \
@@ -62,7 +63,16 @@ apt install -y \
     build-essential \
     calibre \
     cmake \
+    cmake-curses-gui \
     curl \
+    dnsutils \
+    etherwake \
+    ffmpeg \
+    focuswriter \
+    fonts-font-awesome \
+    fonts-fork-awesome \
+    fonts-roboto \
+    fonts-noto \
     gcc \
     geany \
     geany-plugins \
@@ -74,6 +84,7 @@ apt install -y \
     gnome-shell-extension-system-monitor \
     gnome-shell-extension-weather \
     g++ \
+    htop \
     inetutils-tools \
     inkscape \
     ipython \
@@ -82,7 +93,13 @@ apt install -y \
     libboost-all-dev \
     libhdf5-dev \
     libhdf5-openmpi-dev \
+    libhunspell-dev \
     libopenmpi-dev \
+    libqt5concurrent5 \
+    libqt5printsupport5 \
+    libqt5svg5 \
+    libqt5svg5-dev \
+    libqt5webkit5-dev \
     libreoffice-style-breeze \
     libssl-dev \
     lm-sensors \
@@ -93,33 +110,47 @@ apt install -y \
     nodejs \
     pandoc \
     par2 \
+    php-bcmath \
+    php-cli \
+    php-curl \
+    php-xml \
     pkg-config \
     python3 \
     python3-gi \
     python3-matplotlib \
     python3-numpy \
+    python3-odf \
     python3-pytest \
+    python3-sphinx \
     python-matplotlib \
     python-numpy \
     p7zip \
+    qtbase5-dev \
+    qtmultimedia5-dev \
+    qttools5-dev-tools \
+    qt5-default \
     rar \
+    rdesktop \
     rsync \
     screen \
     secure-delete \
     smartmontools \
+    sysstat \
     system-config-printer \
     terminator \
     texlive-full \
     thunar \
     thunderbird \
     thunderbird-l10n-nb-no \
-    typecatcher \
+    ufw \
     unrar \
     vlc \
+    wipe \
     zim
 
 apt purge -y \
-    cups-browsed
+    cups-browsed \
+    unattended-upgrades
 
 echo ""
 echo "****************************************************************************************************"
@@ -149,7 +180,8 @@ if [ ! -f /etc/apt/sources.list.d/spotify.list ]; then
     echo "****************************************************************************************************"
     echo ""
     apt install -y dirmngr
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+#   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
     echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list
     apt update
     apt install -y spotify-client
