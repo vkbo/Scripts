312754840e085b8d44c45f1a178f737f68ccbc24
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2023-02-21 18:51:06 +0100

    Convert a webm file to gif

---
+++ webm2gif.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Source: https://askubuntu.com/a/1350795
+
+ffmpeg -y -i "$1" -vf palettegen _tmp_palette.png
+ffmpeg -y -i "$1" -i _tmp_palette.png -filter_complex paletteuse -r 10  "${1%.webm}.gif"
+rm _tmp_palette.png
