c448b9ca93328a292be6a2a69d9e7147d772d135
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2025-01-31 22:41:33 +0100

    Add script to turn off forced subtitles track for MKV files

---
+++ setMkvNoForced.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Turn off forced subtitles track
+
+for fn in *.mkv; do
+    echo "Processing: $fn"
+    dt=$(stat -c %y "$fn")
+    mkvpropedit "$fn" -e track:s1 -s flag-forced=0 -s flag-default=1
+    touch -m -t $(date -d "$dt" +%Y%m%d%H%M.%S) "$fn"
+done
