45f567ce72645012715a2f2041c2c3e583b66de3
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2025-06-07 12:53:49 +0200

    Fix bug in bulk script

--- cleanMkvTracksBulk.sh
+++ cleanMkvTracksBulk.sh
@@ -2,7 +2,7 @@
 
 for fn in *.mkv; do
     echo "Processing: $fn"
-    out="${1%.*}_clean.mkv"
+    out="${fn%.*}_clean.mkv"
     dt=$(stat -c %y "$fn")
     mkvmerge -o "$out" --atracks "en,no" --stracks "en,nb" "$fn"
     touch -m -t $(date -d "$dt" +%Y%m%d%H%M.%S) "$out"
